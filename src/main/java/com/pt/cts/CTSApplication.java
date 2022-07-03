7e963d0f62a4e0d42bbb06bddb067d0f8db67de0
pingteck <tan.pingteck@gmail.com>
2022-07-03 18:59:05 +0800

    initialise with spring boot
    Initialise Spring Boot
    
    Added dependencies:
    - spring web
    - lombok
    - h2 database

---
+++ src/main/java/com/pt/cts/CTSApplication.java
@@ -0,0 +1,13 @@
+package com.pt.cts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CTSApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CTSApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pt/cts/CTSApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pt.cts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CTSApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
