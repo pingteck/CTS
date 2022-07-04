1b36b9ab6a49933af02e7b81bd199b57c638b45f
pingteck <tan.pingteck@gmail.com>
2022-07-05 00:00:10 +0800

    fix: use JPA

--- src/main/java/com/pt/cts/db/repository/AccountRepository.java
+++ src/main/java/com/pt/cts/db/repository/AccountRepository.java
@@ -1,11 +1,11 @@
 package com.pt.cts.db.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.pt.cts.db.entity.Account;
 
 @Repository
-public interface AccountRepository extends CrudRepository<Account, Long> {
+public interface AccountRepository extends JpaRepository<Account, Long> {
 
 }
